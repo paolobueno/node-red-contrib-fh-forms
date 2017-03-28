2ca0fe455eb780ab6fb43ef8bde89294e3758750
Paolo Bueno <paolohaji@gmail.com>
2017-03-27 21:16:18 -0300

    Add some logging

--- forms-submission-complete/index.js
+++ forms-submission-complete/index.js
@@ -2,14 +2,22 @@ const forms = require('fh-mbaas-api').forms;
 const events = require('events');
 const formsListener = new events.EventEmitter();
 
+const log = console.log.bind(console, 'forms-submission-complete');
+const error = console.error.bind(console, 'forms-submission-complete');
+
 module.exports = function(RED) {
   function formsSubmissionComplete(config) {
     RED.nodes.createNode(this, config);
     var node = this;
     forms.registerListener(formsListener, function(err) {
-      console.error('error registering listener', err);
+      if(err) {
+        error('registering listener', err);
+      } else {
+        log('registered successfully');
+      }
     });
     formsListener.on('submissionComplete', function(data) {
+      log('inside submission complete handler');
       const id = data.submissionId;
       const timestamp = data.submissionCompletedTimestamp;
       const submission = data.submission;
