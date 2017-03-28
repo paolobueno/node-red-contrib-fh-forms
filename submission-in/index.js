b31c002e5edfa095cba878d9e53cf0d85d9b1be5
Paolo Bueno <paolohaji@gmail.com>
2017-03-27 22:24:48 -0300

    Rename submission listening node

--- submission-in/index.js
+++ submission-in/index.js
@@ -2,8 +2,8 @@ const forms = require('fh-mbaas-api').forms;
 const events = require('events');
 const formsListener = new events.EventEmitter();
 
-const log = console.log.bind(console, 'forms-submission-complete');
-const error = console.error.bind(console, 'forms-submission-complete');
+const log = console.log.bind(console, 'forms-submission-in');
+const error = console.error.bind(console, 'forms-submission-in');
 
 module.exports = function(RED) {
   function formsSubmissionComplete(config) {
@@ -30,5 +30,5 @@ module.exports = function(RED) {
       });
     });
   }
-  RED.nodes.registerType('fh-forms-submission-complete', formsSubmissionComplete);
+  RED.nodes.registerType('fh-forms-submission-in', formsSubmissionComplete);
 };
